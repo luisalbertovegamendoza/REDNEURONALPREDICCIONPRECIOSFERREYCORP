3bf7e7d5d79ccf0cb4e4b70c57aa4c61fee33fbc
unknown <luivegmen@gmail.com>
2025-12-19 12:55:27 -0500

    Ajustes de configuración para despliegue en Render

--- PREDICCIONES/settings.py
+++ PREDICCIONES/settings.py
@@ -26,14 +26,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='sannnnnnnnnnnnnnnnn')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = ["*"]
 
-
-
-
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
